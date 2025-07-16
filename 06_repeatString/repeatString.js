8a7020a26b0b6ab690842d0a7ea5ce84d1b97739
Shivank Yadav <194476296+shivankyadav2022@users.noreply.github.com>
2025-07-16 10:11:32 +0530

    make changes to complete javascript ex 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    if (num<0)
+        return "ERROR";
+    let concatedString = '';
+    for(i=0;i<num;i++){
+        concatedString = concatedString+string;
+
+    }
+    return concatedString;
 
 };
 
