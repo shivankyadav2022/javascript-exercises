9ba8c53492640c09cb5c3bcfc314317187c43818
Shivank Yadav <194476296+shivankyadav2022@users.noreply.github.com>
2025-07-17 19:33:53 +0530

    make changes for javascript exercise no 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...arg) {
+
+  for(i in arg){
+  let selectedArgument = arg[i];
+  for(j=0;j<array.length;j++){
+    if(selectedArgument === array[j]){
+      array.splice(j,1);
+      j=0;
+    }
+  }
+}
+ return array;
 };
 
 // Do not edit below this line
