108cc40cf7d0b26b81881f83eca00bf6bfcd386e
Shivank Yadav <194476296+shivankyadav2022@users.noreply.github.com>
2025-07-17 10:20:00 +0530

    make change to complete 07 exercise of javascript

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,4 +1,18 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringToArray= string.split('');
+    const arrayLength = stringToArray.length;
+    let reverseStringArray=[];
+    //reverse string one character at a time
+      for(let i=arrayLength;i>=0;i--){
+         //start iterating the array in reverse and store the elements in new array
+        reverseStringArray.push(stringToArray[i]);
+    }
+   
+    // change the array to string
+    const finalString= reverseStringArray.join('');
+    //return the string
+    return finalString;
+
 
 };
 
