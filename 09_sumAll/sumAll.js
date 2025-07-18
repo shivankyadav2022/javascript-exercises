8398eb2c1fad1cda67533eb89497c25548aaabb2
Shivank Yadav <194476296+shivankyadav2022@users.noreply.github.com>
2025-07-18 19:40:52 +0530

    make changes to complete javascript 09 exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+  let sum =0;
+  let counterInitiator =0;
+  let counterLimit = 10;
+
+  if((typeof a != "number") || (typeof b != "number") || (a<0)||(b<0)|| !(Number.isInteger(a)) || !(Number.isInteger(b)))
+    return 'ERROR';
+    if(b>a){
+      counterInitiator=a;
+      counterLimit=b;
+      }
+    else if(b<a){
+      counterInitiator=b;
+      counterLimit=a;
+    }
+    else
+      return "There are no numbers in between ";
+  for(i=counterInitiator;i<=counterLimit;i++){
+    sum=sum+i;
+
+  }
+  return sum;
 
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
