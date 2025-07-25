ba6d537f8d808681a0081351b737b475f70b9127
Shivank Yadav <194476296+shivankyadav2022@users.noreply.github.com>
2025-07-25 19:58:33 +0530

    make changes for exercise no 11 temp conversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(celsiusTemp) {
+  let fahrenheitTemp = (celsiusTemp-32)*5/9;
+    fahrenheitTemp = Math.round(fahrenheitTemp*10)/10;
+    return fahrenheitTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(fahrenheitTemp) {
+  let celsiusTemp = fahrenheitTemp*9/5+32;
+  celsiusTemp = Math.round(celsiusTemp*10)/10;
+  return celsiusTemp;
 };
 
 // Do not edit below this line
