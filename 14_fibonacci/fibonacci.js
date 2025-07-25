d7e1708762730252deb539763a987eca2a00b601
Shivank Yadav <194476296+shivankyadav2022@users.noreply.github.com>
2025-07-25 20:31:14 +0530

    make changes for ex 13 fiboncacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(typeof num === "string")
+        num = +num;
+    if(num < 0)
+        return "OOPS";
+    if(num === 1)
+        return 1;
+    if(num===0)
+        return 0;
+    let a=0;
+    let b=1;
+    for(let i=0;i<num;i++){
+        sum=a+b;
+        a=b;
+        b=sum;
+        
+    }
+    return a;
 
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
