e409f6a0d188172c6188e0fb2a1e85b11f734b1a
Shivank Yadav <194476296+shivankyadav2022@users.noreply.github.com>
2025-07-25 16:16:19 +0530

    make changes to complete javascirpt exercise no 11 and 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,55 @@
-const add = function() {
+const add = function(a,b) {
+  let sum =0;
+  sum=a+b;
+  return sum;
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+  let subtraction =0;
+  subtraction = a-b;
+  return subtraction;
+
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+  return arr.reduce((sum,item)=>{
+    sum += item;
+    return sum;
+  },0)
 	
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  return arr.reduce((mult,item)=>{
+    mult *= item;
+    return mult;
+  },1)
 
 };
 
-const power = function() {
+const power = function(a,b) {
+  let power = a**b;
+  return power;
 	
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  let fact=1;
+  if(n!=0 && n!=1){
+  for(let i=n;i>1;i--){
+    fact*=i;
+  };
+
+  };
+  return fact;
 	
 };
+
+
+console.log(sum([1,2,3]));
+console.log(multiply([1,2,3]));
 
 // Do not edit below this line
 module.exports = {
